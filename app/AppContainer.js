6b9c8980a849d732f6f8a5d9eb03a5d43e4b0fdf
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2017-08-24 20:42:47 -0500

    initial commit

---
+++ app/AppContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+class AppContainer extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>Dhaval Badhu majama che ne??</Text>
+      </View>
+    );
+  }
+}
+
+export default AppContainer;
